f374e8f6b27e27aebe05b873c447d5036fa71bc4
Zach Johnston <5273704+zachjohnston@users.noreply.github.com>
2018-11-03 13:10:28 -0400

    Create new mvc app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace r2bw_alpha.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
