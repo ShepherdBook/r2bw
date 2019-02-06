9695c1fc1ae7a0a3e668b1a34f40ddb6e241ac73
Zach Johnston <5273704+zachjohnston@users.noreply.github.com>
2019-02-05 20:26:00 -0500

    feat: Scaffold and disable Register

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using r2bw_alpha.Data;
+
+[assembly: HostingStartup(typeof(r2bw_alpha.Areas.Identity.IdentityHostingStartup))]
+namespace r2bw_alpha.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
