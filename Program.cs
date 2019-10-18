958f9eef2bba6d3640b89cab39c615cc853419d9
Zach Johnston <zachjohnston26@gmail.com>
2019-10-18 14:56:01 -0400

    Revert "feat(errors): ✨ Use Sentry"
    This reverts commit e38a1ac567fedd82fe13a3ecb58788a82b4fff58.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Sentry;
 
 namespace r2bw
 {
@@ -8,10 +7,7 @@ namespace r2bw
     {
         public static void Main(string[] args)
         {
-            using (SentrySdk.Init("https://f9dc3b2f25a14cfeb3f38841cb42c7fe@sentry.io/1784237"))
-            {
-                CreateWebHostBuilder(args).Build().Run();
-            }
+            CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
