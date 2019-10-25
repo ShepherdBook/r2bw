013e464160eb0d37ae95bb8ef39733fde4b5b799
Zach Johnston <zachjohnston26@gmail.com>
2019-10-25 13:30:33 -0400

    chore(key vault): more logging

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -132,6 +132,8 @@ namespace r2bw.Areas.Identity.Pages.Account
 
                     if (authorizeResult.Succeeded)
                     {
+                        _logger.LogInformation("User added to a role.");
+
                         var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                         var callbackUrl = Url.Page(
                             "/Account/ConfirmEmail",
@@ -139,6 +141,7 @@ namespace r2bw.Areas.Identity.Pages.Account
                             values: new { userId = user.Id, code },
                             protocol: Request.Scheme);
 
+                        _logger.LogInformation("Sending confirmation email to user... ");
                         await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                             $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
