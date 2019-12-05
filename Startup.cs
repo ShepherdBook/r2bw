0c599be4844bd8e76ad148feed17544332af1d54
Zach Johnston <zachjohnston26@gmail.com>
2019-12-04 22:43:03 -0500

    feat(password): Simplify required password complexity
    Only requirement is: at least 8 characters
    Close #59

--- Startup.cs
+++ Startup.cs
@@ -67,6 +67,16 @@ namespace r2bw
             });
 
             services.AddScoped<SignInManager<User>, Managers.R2bwSignInManager<User>>();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 8;
+                options.Password.RequiredUniqueChars = 1;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
